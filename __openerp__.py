1c0ade149c06456845fdca418a37d8df5ebdab39
fr33co <fr33co@openmailbox.org>
2015-11-29 00:51:39 -0430

    Subida inicial de archivos

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Modulo para prueba.',
+    'version': '8.0.1.0.0',
+    'category': 'Other',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+        Modulo para prueba.
+    """,
+    'author': "Angel A. Guadarrama B.",
+    'website': "https://ve.linkedin.com/in/aguadarrama",
+    'depends': ['base', 'web', 'product', 'sale'],
+    'data': [
+        'view/planificacion.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ model/planificacion.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-#
+
+from openerp import models, fields, api
+
+
+class PlanificacionNivel(models.Model):
+    _name = 'planificacion.nivel'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+
+
+class PlanificacionTipo(models.Model):
+    _name = 'planificacion.tipo'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+
+
+class PlanificacionUnidades(models.Model):
+    _name = 'planificacion.unidades'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+    
+    
+class PlanificacionAsignaturas(models.Model):
+    _name = 'planificacion.asignaturas'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+    nivel_id = fields.Many2one('planificacion.nivel', string="Nivel")
+    horas = fields.Float(string="Horas")
+    
+
+class PlanificacionEjes(models.Model):
+    _name = 'planificacion.ejes'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+    nivel_id = fields.Many2one('planificacion.nivel', string="Nivel")
+    asignatura_id = fields.Many2one('planificacion.asignaturas', string="Asignaturas")
+    
+    
+class PlanificacionCurriculo(models.Model):
+    _name = 'planificacion.curriculo'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+    nivel_id = fields.Many2one('planificacion.nivel', string="Nivel")
+    asignatura_id = fields.Many2one('planificacion.asignaturas', string="Asignaturas")
+    eje_id = fields.Many2one('planificacion.ejes', string="Eje")
+    tipo_id = fields.Many2one('planificacion.tipo', string="Tipo")
+    unidad_id = fields.Many2one('planificacion.unidades', string="Unidad")
+    
+
+class PlanificacionIndicadores(models.Model):
+    _name = 'planificacion.indicadores'
+
+    name = fields.Char(string="Descripción")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+    eje_id = fields.Many2one('planificacion.ejes', string="Eje")
+    unidad_id = fields.Many2one('planificacion.unidades', string="Unidad")
+    curriculo_id = fields.Many2one('planificacion.curriculo', string="Curriculo")
+    
+    
+class PlanificacionClases(models.Model):
+    _name = 'planificacion.clases'
+
+    name = fields.Char(string="Descripción")
+    fecha = fields.Date(string="Fecha")
+    institucion_id = fields.Many2one('res.company', string="Institucion")
+    active = fields.Boolean('Activo?', default=False)
+    nivel_id = fields.Many2one('planificacion.nivel', string="Nivel")
+    asignatura_id = fields.Many2one('planificacion.asignaturas', string="Asignatura")
+    objetivo = fields.Text(string="Objetivo")
